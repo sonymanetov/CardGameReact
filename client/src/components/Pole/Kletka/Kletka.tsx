44b295e5a31f553b793cbfcb70b616a530a9d855
Sony <61905495+sonymanetov@users.noreply.github.com>
2021-02-20 14:52:52 +0300

    ChangesInKletka

--- client/src/components/Pole/Kletka/Kletka.tsx
+++ client/src/components/Pole/Kletka/Kletka.tsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
 
+//объ€вление класса и полей//
+export class CKletka {
+    row: number
+    col: number
+    constructor(x: number, y: number) {
+        this.col = x;
+        this.row = y;
+    }
+}
 
+interface IProps {
+    kletka: CKletka
+}
 
-export default class Kletka extends Component {
+export default class Kletka extends Component<IProps> {
 
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    //тут клетка определ€ет, что тыкнули на неЄ//
+    clickHandler = () => {
+        console.log(`row: ${this.props.kletka.row} col: ${this.props.kletka.col}`);
+    };
+
+    //внешний вид и мастшабирование клетки//
     render() {
         return (
             <div>
-                <Button style={{height:"50px"}}>klik me</Button>
-                
+                <Button onClick={this.clickHandler} style={{ height: "100px" }}>______ ______</Button>
             </div>
         )
     }
