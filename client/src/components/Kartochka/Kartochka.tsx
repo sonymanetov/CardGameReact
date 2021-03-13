03f76a50953389079022346f736d5707ba443515
K-Voland <kosta_v@mail.ru>
2021-03-13 15:43:23 +0300

    CardCreat

--- client/src/components/Kartochka/Kartochka.tsx
+++ client/src/components/Kartochka/Kartochka.tsx
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import heal from '../../assets/heal.jpg';
+import { ICard } from './KardTS';
 
 const useStyles = makeStyles({
   root: {
@@ -19,23 +20,31 @@ const useStyles = makeStyles({
   }
 });
 
-export default function Kartochka() {
-  const classes = useStyles();
+interface IProps {
+  kartochka:ICard;
+}
 
+export default function Kartochka(props: IProps) {
+  const classes = useStyles();
+  const card = props.kartochka;
+// const CardHeal = () => {
   return (
     <Card className={classes.root}>
       <CardActionArea>
       <img src={heal} alt="heal" className = {classes.imgStyle}/>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Аптечка
+            {card.name}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Восстанавливает 50 хр
+            {card.opisanie}
           </Typography>
         </CardContent>
       </CardActionArea>
-
     </Card>
   );
-} 
+}
+
+
+
+// } 
--- client/src/components/Pole/Kletka/Kletka.tsx
+++ client/src/components/Pole/Kletka/Kletka.tsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Kartochka from "../../Kartochka/Kartochka";
 
+//сетка координат
 export class CKletka {
     row: number
     col: number
@@ -15,7 +16,7 @@ export class CKletka {
 interface IProps {
     kletka: CKletka
 }
-
+ // описание клески 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         kletka: {
